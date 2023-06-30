7f98a6b6ad96af8fe6bc0548806b8c8d32ccbbc1
kotegg <lolpol.kek123@gmail.com>
2023-06-30 17:33:11 +0300

    2

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        byte a, b, c;
+        a = b = c = 10;
+        System.out.print(c);
     }
 }
